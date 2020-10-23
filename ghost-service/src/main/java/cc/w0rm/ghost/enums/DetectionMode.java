934fbecf1188978f2f21ad9d306faa963274494a
Ar2oria <ex1stance@outlook.com>
2020-10-23 23:23:11 +0800

    更改hash策略

---
+++ ghost-service/src/main/java/cc/w0rm/ghost/enums/DetectionMode.java
@@ -0,0 +1,20 @@
+package cc.w0rm.ghost.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author : xuyang
+ * @date : 2020/10/23 11:11 下午
+ */
+
+@Getter
+@AllArgsConstructor
+public enum DetectionMode {
+
+    STANDARD,
+    MINIMUM,
+    STRICT
+
+    ;
+}
