4f7eb9e41e22003e5ed7abb2fb74a22ba4b412ee
panyupeng <panyupeng@kkworld.com>
2020-10-26 01:27:54 +0800

    consumer增加非本账号群过滤

--- ghost-service/src/main/java/cc/w0rm/ghost/service/MsgConsumerImpl.java
+++ ghost-service/src/main/java/cc/w0rm/ghost/service/MsgConsumerImpl.java
@@ -1,13 +1,18 @@
 package cc.w0rm.ghost.service;
 
 import cc.w0rm.ghost.api.MsgConsumer;
+import cc.w0rm.ghost.config.AccountManagerConfig;
+import cc.w0rm.ghost.config.role.MsgGroup;
 import com.forte.qqrobot.beans.messages.msgget.MsgGet;
+import com.forte.qqrobot.beans.messages.result.inner.Group;
 import com.forte.qqrobot.bot.BotInfo;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author : xuyang
@@ -21,10 +26,7 @@ import java.util.Set;
 @Slf4j
 @Service("q1")
 public class MsgConsumerImpl implements MsgConsumer {
-
-    private static final Set<String> TEST_GROUP_CODES = Sets.newHashSet( "830628164");
-
-
+    
     /**
      * @param botInfo 账号信息
      * @param group   qq群号
@@ -32,14 +34,11 @@ public class MsgConsumerImpl implements MsgConsumer {
      */
     @Override
     public void consume(BotInfo botInfo, String group, MsgGet msgGet) {
-        log.debug("[q1] 消费者：[{}] , 接收到消息：[{}] ==> 群qq：[{}]",
-                botInfo.getBotCode(),
-                msgGet.getId(),
-                group);
-
-        if (TEST_GROUP_CODES.contains(group)) {
+        log.debug("[q1] 消费者：[{}] , 接收到消息：[{}] ==> 群qq：[{}]", botInfo.getBotCode(), msgGet.getId(), group);
+        Set<String> botGroups = botInfo.getSender().GETTER.getGroupList().stream().map(Group::getCode)
+            .collect(Collectors.toSet());
+        if (botGroups.contains(group)) {
             botInfo.getSender().SENDER.sendGroupMsg(group, msgGet.getMsg());
         }
     }
-
 }
