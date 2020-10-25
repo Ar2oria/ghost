a1990bf76d5b43caa9932fe14b03b41443d0f88a
Ar2oria <ex1stance@outlook.com>
2020-10-25 17:26:17 +0800

    start.sh 更新命令

--- start.sh
+++ start.sh
@@ -15,6 +15,6 @@ if [ $process -ge 1 ];then
    ps axu | grep ${module_name} |grep -v grep |awk '{print $2}'| xargs kill
 fi
 
-nohup java -jar ghost-main/target/ghost-main.jar --XX:+UseG1G >/dev/null 2>&1&
+exec nohup java -jar ghost-main/target/ghost-main.jar --XX:+UseG1G >/dev/null 2>&1&
 
 ps aux|grep ${module_name} |grep -v grep
