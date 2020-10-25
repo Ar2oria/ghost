d669000bede1cb692e5ba8f3aea06869ffd17a4d
Ar2oria <ex1stance@outlook.com>
2020-10-25 17:29:03 +0800

    调整脚本执行顺序

--- start.sh
+++ start.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-git checkout master
-git branch -D online
-
-git fetch
-git checkout origin/online -b online
-git branch -v
-mvn clean package -Dmaven.skip.test=true
-
 module_name="ghost-main.jar"
 
 process=`ps axu|grep ${module_name} |grep -v grep |awk '{print $2}'|wc -l`
 if [ $process -ge 1 ];then
    ps axu | grep ${module_name} |grep -v grep |awk '{print $2}'| xargs kill
 fi
+
+
+git checkout master
+git branch -D online
+
+git fetch
+git checkout origin/online -b online
+git branch -v
+
+mvn clean package -Dmaven.skip.test=true
 
 exec nohup java -jar ghost-main/target/ghost-main.jar --XX:+UseG1G >/dev/null 2>&1&
 
